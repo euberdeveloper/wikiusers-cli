1e0b4dc7bdf04b847177b9496a1b5d230f6db2fe
euberdeveloper <euberdeveloper@gmail.com>
2021-07-06 00:30:43 +0200

    refactor: add utils
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- wikiusers-cli/main.py
+++ wikiusers-cli/main.py
@@ -1,31 +1,11 @@
 import click
-from whaaaaat import prompt
 
 from wikiusers import settings
 from wikiusers.dataloader import WhdtLoader
 from wikiusers.rawprocessor import RawProcessor
 from wikiusers.postprocessor import PostProcessor
 
-
-def select_languages(available_langs: list[str], current_langs: list[str]) -> list[str]:
-    current_langs = [l for l in current_langs]
-
-    include_all = 'all' in current_langs
-    if include_all:
-        current_langs.remove('all')
-
-    options = [
-        {'name': option, 'value': option,
-            'checked': include_all or option in current_langs}
-        for option in available_langs
-    ]
-    answer = prompt({
-        'name': 'langs',
-        'message': 'Select the languages to purge',
-        'type': 'checkbox',
-        'choices': options
-    })
-    return answer['langs']
+from .utils import select_languages
 
 
 @click.group(help="Tool to analyze the wikimedia history dump tsv and obtain per-user information")
